bd66da5191fedca90592e4112ee3ae9c3714f782
FIX PRICE <RETAIL@Deniss-MacBook-Pro.local>
2024-10-07 15:37:11 +0700

    Initial commit

---
+++ iWord/iWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWordApp.swift
+//  iWord
+//
+//  Created by FIX PRICE on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
