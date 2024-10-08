55fba853ebc208b8eedf2c1b4d6175b1b667cdfa
FIX PRICE <RETAIL@Deniss-MacBook-Pro.local>
2024-10-08 19:28:10 +0700

    Relocate files

--- iWord/FolderContentScreen/FolderContentView.swift
+++ iWord/FolderContentScreen/FolderContentView.swift
@@ -11,40 +11,6 @@
 import UIKit
 import SnapKit
 
-struct LexicalUnit {
-    let mainTranslation: String
-    
-    init() {
-        self.mainTranslation = "Text"
-    }
-}
-
-final class LexicalUnitCell: UITableViewCell {
-    private(set) var lexicalUnit: LexicalUnit?
-    private let mainTranslationTitle = UILabel()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = .blue
-        contentView.addSubview(mainTranslationTitle)
-        mainTranslationTitle.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
-            make.top.equalToSuperview()
-            make.bottom.equalToSuperview()
-        }
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setLexicalUnit(_ lexicalUnit: LexicalUnit) {
-        self.lexicalUnit = lexicalUnit
-        mainTranslationTitle.text = lexicalUnit.mainTranslation
-    }
-}
-
 final class FolderContentView: UIView {
     private let listOfUnits: [LexicalUnit] = [LexicalUnit(), LexicalUnit()]
     private let tableView = UITableView()
---
+++ iWord/FolderContentScreen/LexicalUnitCell.swift
@@ -0,0 +1,38 @@
+// LexicalUnitCell.swift
+// iWord
+//
+// Created by Denis Ulianov on 10/8/24.
+// LinkedIn: https://www.linkedin.com/in/denis-u-926707256/
+// Copyright © 2024 . All rights reserved.
+//
+// This project is part of the dissertation research for the 09.04.03 curriculum at Tambov State University.
+// University website: https://tambovstateuniversity.org 
+
+import UIKit
+import SnapKit
+
+final class LexicalUnitCell: UITableViewCell {
+    private(set) var lexicalUnit: LexicalUnit?
+    private let mainTranslationTitle = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .blue
+        contentView.addSubview(mainTranslationTitle)
+        mainTranslationTitle.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setLexicalUnit(_ lexicalUnit: LexicalUnit) {
+        self.lexicalUnit = lexicalUnit
+        mainTranslationTitle.text = lexicalUnit.mainTranslation
+    }
+}
