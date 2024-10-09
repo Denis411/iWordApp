3af69c42d6b17e57adcee43ca6e7d0363d6a9ffa
FIX PRICE <RETAIL@Deniss-MacBook-Pro.local>
2024-10-09 11:17:39 +0700

    Correct UITableViewCell style

--- iWord/FolderContentScreen/LexicalUnitCell.swift
+++ iWord/FolderContentScreen/LexicalUnitCell.swift
@@ -13,18 +13,40 @@ import SnapKit
 
 final class LexicalUnitCell: UITableViewCell {
     private(set) var lexicalUnit: LexicalUnit?
-    private let mainTranslationTitle = UILabel()
+    private let originalWordLabel = UILabel()
+    private let mainTranslationLabel = UILabel()
+    private let verticalStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = .blue
-        contentView.addSubview(mainTranslationTitle)
-        mainTranslationTitle.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
-            make.top.equalToSuperview()
-            make.bottom.equalToSuperview()
+        contentView.addSubview(verticalStack)
+        [originalWordLabel, mainTranslationLabel].forEach { label in
+            label.font = .systemFont(ofSize: 20, weight: .medium)
+            label.numberOfLines = 1
         }
+        
+        verticalStack.addArrangedSubview(originalWordLabel)
+        verticalStack.addArrangedSubview(mainTranslationLabel)
+        verticalStack.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20)
+            make.trailing.equalToSuperview()
+            make.verticalEdges.equalToSuperview().inset(20)
+        }
+        contentView.layer.backgroundColor = UIColor.white.cgColor
+        contentView.layer.cornerRadius = 12
+        
+        contentView.clipsToBounds = false
+        contentView.layer.masksToBounds = false
+        
+        contentView.layer.shadowColor = UIColor.gray.cgColor
+        contentView.layer.shadowRadius = 12
+        contentView.layer.shadowOpacity = 0.5
+        contentView.layer.shadowOffset = .init(width: 2, height: 2)
     }
     
     required init?(coder: NSCoder) {
@@ -33,6 +55,7 @@ final class LexicalUnitCell: UITableViewCell {
     
     func setLexicalUnit(_ lexicalUnit: LexicalUnit) {
         self.lexicalUnit = lexicalUnit
-        mainTranslationTitle.text = lexicalUnit.mainTranslation
+        originalWordLabel.text = lexicalUnit.originalWord
+        mainTranslationLabel.text = lexicalUnit.mainTranslation
     }
 }
